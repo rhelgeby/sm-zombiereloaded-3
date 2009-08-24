27a29220e2e99ac80da212f98dd362d48c67a826
richard <devnull@localhost>
2009-08-24 23:48:28 +0200

    Updated script for dumping version info, and new windows batch file.

--- updateversion.sh
+++ updateversion.sh
@@ -1,10 +1,19 @@
-#/bin/sh
+#!/bin/sh
+
+# Program for printing date.
+ZR_DATEPATH=date
+
+if [ -n $1 ]
+then
+    ZR_DATEPATH=$1
+fi
 
 ZR_VERSION_FILE="src/zr/hgversion.h.inc"
 
 ZR_PRODUCT_NAME="Zombie:Reloaded"
 ZR_COPYRIGHT="Copyright (C) 2009  Greyscale, Richard Helgeby"
 ZR_LICENSE="GNU GPL, Version 3"
+ZR_DATE=$($ZR_DATEPATH -R)
 
 echo "#define ZR_VER_PRODUCT_NAME     \"$ZR_PRODUCT_NAME\"" > $ZR_VERSION_FILE
 echo "#define ZR_VER_COPYRIGHT        \"$ZR_COPYRIGHT\"" >> $ZR_VERSION_FILE
@@ -12,4 +21,6 @@ echo "#define ZR_VER_VERSION          VERSION" >> $ZR_VERSION_FILE
 echo "#define ZR_VER_BRANCH           \"$(hg id -b)\"" >> $ZR_VERSION_FILE
 echo "#define ZR_VER_REVISION         \"$(hg id -n):$(hg id -i)\"" >> $ZR_VERSION_FILE
 echo "#define ZR_VER_LICENSE          \"$ZR_LICENSE\"" >> $ZR_VERSION_FILE
-echo "#define ZR_VER_DATE             \"$(date)\"" >> $ZR_VERSION_FILE
+echo "#define ZR_VER_DATE             \"$ZR_DATE\"" >> $ZR_VERSION_FILE
+
+echo "Updated hgversion.h.inc"
