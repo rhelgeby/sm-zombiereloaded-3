b2b077c9167a4dff2b9fd916c46baea4cb2be6b7
richard <devnull@localhost>
2009-08-26 04:44:59 +0200

    Updated scripts for dumping version info.

--- updateversion.bat
+++ updateversion.bat
@@ -1,4 +1,14 @@
-set BASH=..\zr-devenv\win32\bin\bash.exe
-set DATEPATH=../zr-devenv/win32/bin/date.exe
+@echo off
+REM Note: Copy this script to the source code repositroy and execute it
+REM       from that location.
+
+REM To avoid complicated and maybe impossible tasks in windows batch scripts,
+REM this script will execute another linux shell script using cygwin.
+
+set BASH=%ZRDEVENV%\win32\bin\bash.exe
+set DATEPATH=%ZRDEVENV%\win32\bin\date.exe
+
+REM Converts a windows path to a linux path for cygwin (/cygdrive/c/...).
+for /f %%s in ('%ZRDEVENV%\win32\bin\cygpath.exe -u "%DATEPATH%"') do set DATEPATH=%%s
 
 %BASH% updateversion.sh %DATEPATH%
