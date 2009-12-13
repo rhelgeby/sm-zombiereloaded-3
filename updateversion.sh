917a4ce1e3a56666c8c225989a4ebe605690d716
richard <devnull@localhost>
2009-12-14 00:39:21 +0100

    Changed to default branch, with hard coded branch in version script.

--- updateversion.sh
+++ updateversion.sh
@@ -15,13 +15,15 @@ ZR_VERSION_FILE="src/zr/hgversion.h.inc"
 
 ZR_PRODUCT_NAME="Zombie:Reloaded"
 ZR_COPYRIGHT="Copyright (C) 2009  Greyscale, Richard Helgeby"
+ZR_BRANCH="zr-3.0-dev"
 ZR_LICENSE="GNU GPL, Version 3"
 ZR_DATE=$($ZR_DATEPATH -R)
 
 echo "#define ZR_VER_PRODUCT_NAME     \"$ZR_PRODUCT_NAME\"" > $ZR_VERSION_FILE
 echo "#define ZR_VER_COPYRIGHT        \"$ZR_COPYRIGHT\"" >> $ZR_VERSION_FILE
 echo "#define ZR_VER_VERSION          VERSION" >> $ZR_VERSION_FILE
-echo "#define ZR_VER_BRANCH           \"$(hg id -b)\"" >> $ZR_VERSION_FILE
+echo "#define ZR_VER_BRANCH           \"$ZR_BRANCH\"" >> $ZR_VERSION_FILE
+#echo "#define ZR_VER_BRANCH           \"$(hg id -b)\"" >> $ZR_VERSION_FILE
 echo "#define ZR_VER_REVISION         \"$(hg id -n):$(hg id -i)\"" >> $ZR_VERSION_FILE
 echo "#define ZR_VER_LICENSE          \"$ZR_LICENSE\"" >> $ZR_VERSION_FILE
 echo "#define ZR_VER_DATE             \"$ZR_DATE\"" >> $ZR_VERSION_FILE
